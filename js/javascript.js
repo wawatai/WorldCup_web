d8c2d555f1b30fe6e12754b1bb4b41683a4e6866
wataiPublic@gmail.com <wataiPublic@gmail.com>
2022-11-01 14:57:57 +0800

    111-1101-2

--- js/javascript.js
+++ js/javascript.js
@@ -11,3 +11,66 @@ $(function(){
         .addClass("style_"+ (n + 1) +"");
     })
 })
+
+$(function(){
+    var gh = $(".mainArea .gameBox");
+    var bh = $(".mainArea .bettingBox");
+    var wh = window.innerHeight;
+
+    gh.css("height", (String(wh - 273) + "px"));
+    bh.css("height", (String(wh - 469) + "px"));
+
+    var bhRe = bh.height();
+    var oh = $(".mainArea .outerBox");
+
+    oh.css("height", (String(bhRe - 58) + "px"));
+
+    $(window).resize(function () {
+        var gh = $(".mainArea .gameBox");
+        var bh = $(".mainArea .bettingBox");
+        var wh = window.innerHeight;
+
+        gh.css("height", (String(wh - 273) + "px"));
+        bh.css("height", (String(wh - 469) + "px"));
+
+        var bhRe = bh.height();
+        var oh = $(".mainArea .outerBox");
+
+        oh.css("height", (String(bhRe - 58) + "px"));
+    });
+});
+
+$(function(){
+    var cw = $("header .center");
+    var ww = window.innerWidth;
+
+    if(ww > 1450)
+    {
+        cw.css("width", (String(839) + "px"));
+    }
+    else if(ww <= 1450 && ww > 1300)
+    {
+        cw.css("width", (String(ww - 610) + "px"));
+    }
+    else
+    {
+        cw.css("width", (String(691) + "px"));
+    }
+
+    $(window).resize(function () {
+        var cw = $("header .center");
+        var ww = window.innerWidth;
+        if(ww > 1450)
+        {
+            cw.css("width", (String(839) + "px"));
+        }
+        else if(ww <= 1450 && ww > 1300)
+        {
+            cw.css("width", (String(ww - 610) + "px"));
+        }
+        else
+        {
+            cw.css("width", (String(691) + "px"));
+        }
+    });
+})
